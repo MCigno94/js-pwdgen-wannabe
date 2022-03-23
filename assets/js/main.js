dd07a3be18a1e341af5de9a4adc81cde3d409010
MCigno94 <mattiacigno27@gmail.com>
2022-03-23 15:24:03 +0100

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+// Stampare una psw contenente nome + cognome + colore + n° a scelta
+
+// Creare le variabili firstName - lastName - color e psw
+
+// Stampare il risultato in console
+
+// Stampare il risultato in pagina
