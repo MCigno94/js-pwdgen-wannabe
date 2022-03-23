0959b3872c56ddc55a3e50d5a90285df21940fb1
MCigno94 <mattiacigno27@gmail.com>
2022-03-23 15:27:03 +0100

    final Commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,13 @@
 // Stampare una psw contenente nome + cognome + colore + n° a scelta
 
 // Creare le variabili firstName - lastName - color e psw
+const firstName = prompt("Type your name");
+const lastName = prompt("Type your surname");
+const color = prompt("Type your favorite color");
+const psw = firstName + lastName + color + 27
 
 // Stampare il risultato in console
+console.log(`${psw}`);
 
 // Stampare il risultato in pagina
+document.getElementById("psw_generator").innerHTML = psw;
